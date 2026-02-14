d9232dd4274f86ea169128470faf9ea55c2ecb7a
Kmilo <kmilogp8496@gmail.com>
2026-02-14 11:15:17 +0100

    Added live grep args to neovim

--- init.lua
+++ init.lua
@@ -354,6 +354,8 @@ require('lazy').setup({
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+
+      'nvim-telescope/telescope-live-grep-args.nvim',
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
